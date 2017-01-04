d14808fa5950ecd882846b13c93ae37078447b7b
Lukas Reimer <Luki@Lukis-MBP.local>
2017-01-04 11:41:59 +0100

    major restructuring of source and header files into a more common folder structure and switching to cmake

---
+++ build/CMakeFiles/test_adjmatrix.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Luki/CSE/WiSe16/EPA /Übung/Übung06-Graph/src/adjmatrix.c" "/Users/Luki/CSE/WiSe16/EPA /Übung/Übung06-Graph/build/CMakeFiles/test_adjmatrix.dir/src/adjmatrix.c.o"
+  "/Users/Luki/CSE/WiSe16/EPA /Übung/Übung06-Graph/test/test_adjmatrix.c" "/Users/Luki/CSE/WiSe16/EPA /Übung/Übung06-Graph/build/CMakeFiles/test_adjmatrix.dir/test/test_adjmatrix.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_adjmatrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_adjmatrix.dir/test/test_adjmatrix.c.o"
+  "CMakeFiles/test_adjmatrix.dir/src/adjmatrix.c.o"
+  "test_adjmatrix.pdb"
+  "test_adjmatrix"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_adjmatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
